c7e8ae59ff1e8bc42d6f95b29bea3517ea7f494d
Holden Offmenn <holden.luz@grupoicts.com.br>
2024-05-20 15:22:35 -0300

    Tela de solicitaçãoes com filtro

--- app/lib/solicitacoes_page.dart
+++ app/lib/solicitacoes_page.dart
@@ -11,6 +11,9 @@ class SolicitacoesPage extends StatefulWidget {
 class _SolicitacoesPageState extends State<SolicitacoesPage> {
   late WebSocketService _webSocketService;
   List<dynamic> _solicitacoes = [];
+  List<dynamic> _filteredSolicitacoes = [];
+  String _searchQuery = '';
+  String _selectedStatus = 'Todos';
 
   @override
   void initState() {
@@ -20,6 +23,7 @@ class _SolicitacoesPageState extends State<SolicitacoesPage> {
       if (data['action'] == 'solicitacoesData') {
         setState(() {
           _solicitacoes = data['data'];
+          _filteredSolicitacoes = _solicitacoes;
         });
       }
     });
@@ -33,54 +37,162 @@ class _SolicitacoesPageState extends State<SolicitacoesPage> {
     super.dispose();
   }
 
+  void _filterSolicitacoes() {
+    setState(() {
+      _filteredSolicitacoes = _solicitacoes.where((solicitacao) {
+        final matchesStatus = _selectedStatus == 'Todos' || solicitacao['status'] == _selectedStatus;
+        final matchesQuery = solicitacao['linha_solicitante'].toLowerCase().contains(_searchQuery.toLowerCase());
+        return matchesStatus && matchesQuery;
+      }).toList();
+    });
+  }
+
+  IconData getStatusIcon(String status) {
+    switch (status) {
+      case 'ABERTO':
+        return Icons.radio_button_unchecked;
+      case 'FECHADO':
+        return Icons.check_circle;
+      case 'FAZENDO':
+        return Icons.hourglass_bottom;
+      default:
+        return Icons.help;
+    }
+  }
+
+  Color getStatusColor(String status) {
+    switch (status) {
+      case 'ABERTO':
+        return Colors.orange;
+      case 'FECHADO':
+        return Colors.green;
+      case 'FAZENDO':
+        return Colors.blue;
+      default:
+        return Colors.grey;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Solicitações'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: ListView.builder(
-        itemCount: _solicitacoes.length,
-        itemBuilder: (context, index) {
-          final solicitacao = _solicitacoes[index];
-          final DateTime horaSolicitacao = DateTime.parse(solicitacao['hora_solicitacao']);
-          final String horaFormatada = DateFormat('HH:mm').format(horaSolicitacao);
-          final bool isToday = DateFormat('yyyy-MM-dd').format(horaSolicitacao) == DateFormat('yyyy-MM-dd').format(DateTime.now());
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Pesquisar...',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    onChanged: (query) {
+                      setState(() {
+                        _searchQuery = query;
+                        _filterSolicitacoes();
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(width: 10),
+                DropdownButton<String>(
+                  value: _selectedStatus,
+                  items: <String>['Todos', 'ABERTO', 'FECHADO', 'FAZENDO']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedStatus = newValue!;
+                      _filterSolicitacoes();
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: _filteredSolicitacoes.length,
+                itemBuilder: (context, index) {
+                  final solicitacao = _filteredSolicitacoes[index];
+                  final DateTime horaSolicitacao = DateTime.parse(solicitacao['hora_solicitacao']);
+                  final String horaFormatada = DateFormat('HH:mm').format(horaSolicitacao);
+                  final bool isToday = DateFormat('yyyy-MM-dd').format(horaSolicitacao) ==
+                      DateFormat('yyyy-MM-dd').format(DateTime.now());
 
-          IconData getStatusIcon(String status) {
-            switch (status) {
-              case 'ABERTO':
-                return Icons.radio_button_unchecked;
-              case 'FECHADO':
-                return Icons.check_circle;
-              case 'FAZENDO':
-                return Icons.hourglass_bottom;
-              default:
-                return Icons.help;
-            }
-          }
-
-          return Card(
-            child: ListTile(
-              leading: Icon(getStatusIcon(solicitacao['status'])),
-              title: Text(
-                solicitacao['linha_solicitante'],
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('ID: ${solicitacao['id_solicitacao']}'),
-                  Text('Hora: $horaFormatada'),
-                  if (isToday)
-                    Text('Hoje', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),
-                  if (!isToday)
-                    Text('Antiga', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
-                ],
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    elevation: 5,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: getStatusColor(solicitacao['status']),
+                        child: Icon(
+                          getStatusIcon(solicitacao['status']),
+                          color: Colors.white,
+                        ),
+                      ),
+                      title: Text(
+                        solicitacao['linha_solicitante'],
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('ID: ${solicitacao['id_solicitacao']}'),
+                              Text('Hora: $horaFormatada'),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                decoration: BoxDecoration(
+                                  color: isToday ? Colors.green : Colors.red,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Text(
+                                  isToday ? 'Hoje' : 'Antiga',
+                                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      trailing: Icon(
+                        getStatusIcon(solicitacao['status']),
+                        color: getStatusColor(solicitacao['status']),
+                        size: 30,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
